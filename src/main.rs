02b83d277a7aa2f686d4a9c12e5a51116cc18132
Renne Sampaio <rennesampaio97@gmail.com>
2019-01-02 10:10:06 -0300

    Chapter 3: gradient background

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,50 @@ use std::path::Path;
 
 use png::HasParameters;
 
+mod ray;
 mod vec3;
 
+use crate::ray::Ray;
 use crate::vec3::Vec3;
 
 fn main() {
     const WIDTH: u32 = 200;
     const HEIGHT: u32 = 100;
 
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::left() * 4.0;
+    let vertical = Vec3::up() * 2.0;
+    let origin = Vec3::zero();
+
     let mut data = Vec::<u8>::with_capacity((4 * WIDTH * HEIGHT) as usize);
-    let d_height = 255 / HEIGHT;
-    let d_width = 255 / WIDTH;
 
     for y in (0..HEIGHT).rev() {
         for x in 0..WIDTH {
-            let v = Vec3::new(
-                (d_width * x) as f32,
-                (d_height * y) as f32,
-                (255 / 5) as f32,
-            );
+            let u = x as f32 / WIDTH as f32;
+            let v = y as f32 / HEIGHT as f32;
 
-            data.push(v[0] as u8);
-            data.push(v[1] as u8);
-            data.push(v[2] as u8);
+            let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+
+            let col = color(&ray);
+
+            data.push((255.0 * col.r()) as u8);
+            data.push((255.0 * col.g()) as u8);
+            data.push((255.0 * col.b()) as u8);
             data.push(255);
         }
     }
 
     write_image("test.png", WIDTH, HEIGHT, &data);
+}
+
+fn color(ray: &Ray) -> Vec3 {
+    let dir = ray.direction;
+
+    // Puts t in the range 0..1
+    let t = 0.5 * (dir.y() + 1.0);
+
+    // Gradient from blue to white
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
 
 fn write_image(path: &str, width: u32, height: u32, data: &[u8]) {
